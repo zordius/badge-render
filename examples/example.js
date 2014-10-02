4972122c643596505e6eed19432d177feeb11434
Zordius Chen <zordius@yahoo-inc.com>
2014-10-02 09:15:53 +0800

    add example

---
+++ examples/example.js
@@ -0,0 +1,8 @@
+require('..')({
+   safari: {'10.0': true},
+   explorer: {'10.0': false},
+   firefox: {'10.0': true},
+   chrome: {'10.0': true},
+   ipad: {'10.0': true},
+   iphone: {'10.0': true}
+});
